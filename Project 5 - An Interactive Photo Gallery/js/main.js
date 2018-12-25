be6cd4bf7cf7e950d2b6efc1f5e16f86d1de7a65
Vincent Man <vincentman2702@hotmail.com>
2018-12-25 20:20:49 +0000

    Project 5 - An Interactive Photo Gallery

--- Project 5 - An Interactive Photo Gallery/js/main.js
+++ Project 5 - An Interactive Photo Gallery/js/main.js
@@ -1,9 +1,20 @@
-$("#search-bar").keyup(function() {
-  var val = $.trim(this.value);
-  if (val === "")
-      $('img').show();
-  else {
-    $('img').hide();
-    $("img[alt*=" + val + " i]").show();
-  }
-});
+// Declare variables
+const img = document.querySelectorAll("img");
+const search_input = document.querySelector("input");
+
+search_input.addEventListener("keyup", function() {
+    // Get the search input string to lowercase
+    const val = search_input.value.toLowerCase();
+    // To loop through the images' caption texts
+    for ( var i=0; i<img.length; i++) {
+      const caption = img[i].getAttribute("alt");
+      // To check if the search input value is in the selected caption
+      if ( caption.toLowerCase().indexOf(val) > -1 ) {
+        // Show image(s) if the value matches
+        img[i].style.display = 'flex';
+      } else {
+        // Otherwise images will not be displayed
+        img[i].style.display = 'none';
+      }
+    }
+  });
