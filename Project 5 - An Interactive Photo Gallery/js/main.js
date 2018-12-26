6e50f01960b42bed60758db46f2c758a4119de4e
Vincent Man <vincentman2702@hotmail.com>
2018-12-26 00:06:03 +0000

    Project 5 - An Interactive Photo Gallery

--- Project 5 - An Interactive Photo Gallery/js/main.js
+++ Project 5 - An Interactive Photo Gallery/js/main.js
@@ -1,17 +1,17 @@
 // Declare variables
-const img = document.querySelectorAll("img");
-const search_input = document.querySelector("input");
+const search_input = document.getElementById("search-bar");
+const img = document.getElementsByTagName("img");
 
 search_input.addEventListener("keyup", function() {
     // Get the search input string to lowercase
     const val = search_input.value.toLowerCase();
     // To loop through the images' caption texts
-    for ( var i=0; i<img.length; i++) {
+    for ( var i=0; i<img.length; i++ ) {
       const caption = img[i].getAttribute("alt");
       // To check if the search input value is in the selected caption
       if ( caption.toLowerCase().indexOf(val) > -1 ) {
         // Show image(s) if the value matches
-        img[i].style.display = 'flex';
+        img[i].style.display = '';
       } else {
         // Otherwise images will not be displayed
         img[i].style.display = 'none';
